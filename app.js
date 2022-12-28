295491317e51f84f3540c4fd15ab3055c3e0180f
Roman Serediuk <box4rom@gmail.com>
2022-12-28 13:59:31 +0200

    create and connect users router

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import logger from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import usersRouter from './routes/users.js';
+
 dotenv.config();
 
 const app = express();
@@ -12,6 +14,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', usersRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });

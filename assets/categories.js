a8968621ad18f8b6bf07f12e239e956ae52ad298
Roman Serediuk <box4rom@gmail.com>
2022-12-28 18:00:51 +0200

    create categories collection

---
+++ assets/categories.js
@@ -0,0 +1,54 @@
+const categories = [
+    {
+        id: 1,
+        value: 'Main expenses',
+        color: '#FED057',
+    },
+    {
+        id: 2,
+        value: 'Products',
+        color: '#FFD8D0',
+    },
+    {
+        id: 3,
+        value: 'Car',
+        color: '#FD9498',
+    },
+    {
+        id: 4,
+        value: 'Self care',
+        color: '#C5BAFF',
+    },
+    {
+        id: 5,
+        value: 'Child care',
+        color: '#6E78E8',
+    },
+    {
+        id: 6,
+        value: 'Child care',
+        color: '#6E78E8',
+    },
+    {
+        id: 7,
+        value: 'Household products',
+        color: '#4A56E2',
+    },
+    {
+        id: 8,
+        value: 'Education',
+        color: '#81E1FF',
+    },
+    {
+        id: 9,
+        value: 'Leisure',
+        color: '#24CCA7',
+    },
+    {
+        id: 10,
+        value: 'Other expenses',
+        color: '#00AD84',
+    },
+];
+
+export default categories;

ecf168b12357696bab7e4b6da3478262e75ed182
Roman Serediuk <box4rom@gmail.com>
2022-12-29 16:31:45 +0200

    update success response

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,8 +2,15 @@ import { transactionServices } from '../services/transactions.js';
 
 const create = async (req, res, next) => {
     const { _id } = req.user;
-    await transactionServices.createTransaction(req.body, _id);
-    res.json(req.body);
+    const operation = await transactionServices.createTransaction(
+        req.body,
+        _id
+    );
+    res.status(201).json({
+        data: {
+            operation,
+        },
+    });
 };
 
 export const transactionsControllers = {

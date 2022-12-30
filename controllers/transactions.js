b9e397fdcc5a09b71ffef16d943610dd51642bf2
Roman Serediuk <box4rom@gmail.com>
2022-12-30 14:54:06 +0200

    feature - create new transaction with actualBalance field

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,9 +1,32 @@
 import { transactionServices } from '../services/transactions.js';
+import Transaction from '../schemas/transaction.js';
 
-const create = async (req, res, next) => {
+const findLastTransactionRecord = id => {
+    return Transaction.aggregate([
+        {
+            $match: {
+                owner: id,
+            },
+        },
+        {
+            $sort: {
+                createdAt: -1,
+            },
+        },
+        {
+            $limit: 1,
+        },
+    ]);
+};
+
+const create = async (req, res) => {
     const { _id } = req.user;
+    const lastRecord = await findLastTransactionRecord(_id);
+    const balance = lastRecord[0]?.actualBalance || 0;
+
+    const newTransaction = createNewTransactionObject(req.body, _id, balance);
     const operation = await transactionServices.createTransaction(
-        req.body,
+        newTransaction,
         _id
     );
     res.status(201).json({
@@ -11,6 +34,23 @@ const create = async (req, res, next) => {
             operation,
         },
     });
+};
+
+const createNewTransactionObject = (data, userId, balance) => {
+    const { type, category = 'income', amount, date = new Date() } = data;
+
+    let actualBalance = type === 'income' ? balance + amount : balance - amount;
+
+    let newTransaction = {
+        owner: userId,
+        type,
+        category,
+        amount,
+        date,
+        actualBalance,
+    };
+
+    return newTransaction;
 };
 
 export const transactionsControllers = {

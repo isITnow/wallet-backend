8cbb961e7f01a7a8e947c4449a3b089b0ede1d4e
Roman Serediuk <box4rom@gmail.com>
2023-01-02 11:39:00 +0200

    create get transactions by date  rote-controller-srvise

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,7 +6,11 @@ import { asyncWrapper } from '../utils/asyncWrapper.js';
 const router = express.Router();
 
 router.use(authMiddleware);
-router.post('/', asyncWrapper(transactionsControllers.create));
+router.post('/', asyncWrapper(transactionsControllers.createTransaction));
 router.get('/', asyncWrapper(transactionsControllers.getAllTransactions));
+router.get(
+    '/:month/:year',
+    asyncWrapper(transactionsControllers.getTransactionsByDate)
+);
 
 export default router;

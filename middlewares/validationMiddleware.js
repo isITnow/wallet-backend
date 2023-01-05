dcc7203c8f514cf234e22b97aa8bed8ab29add14
Roman Serediuk <box4rom@gmail.com>
2023-01-05 21:40:19 +0200

    bufix - validation email, password, name

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -3,15 +3,22 @@ const Joi = require('joi');
 module.exports = {
     addUserValidation: (req, res, next) => {
         const schema = Joi.object({
-            name: Joi.string().alphanum().min(2).max(24).required(),
+            name: Joi.string().min(1).max(12).required(),
             email: Joi.string()
-                .email({
-                    minDomainSegments: 2,
-                    tlds: { allow: ['com', 'net'] },
-                })
+                .pattern(
+                    new RegExp('^(?:w+[-.])*w{2,63}@(?:w+[-.])*w+.w{2,4}$')
+                )
+                // .email({
+                //     minDomainSegments: 2,
+                //     tlds: { allow: ['com', 'net'] },
+                // })
                 .required(),
             password: Joi.string()
-                .pattern(new RegExp('^[a-zA-Z0-9]{6,16}$'))
+                .pattern(
+                    new RegExp(
+                        '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,12}$'
+                    )
+                )
                 .required(),
         });
         const validationResult = schema.validate(req.body);
@@ -27,13 +34,20 @@ module.exports = {
     loginUserValidation: (req, res, next) => {
         const schema = Joi.object({
             email: Joi.string()
-                .email({
-                    minDomainSegments: 2,
-                    tlds: { allow: ['com', 'net'] },
-                })
+                .pattern(
+                    new RegExp('^(?:w+[-.])*w{2,63}@(?:w+[-.])*w+.w{2,4}$')
+                )
+                // .email({
+                //     minDomainSegments: 2,
+                //     tlds: { allow: ['com', 'net'] },
+                // })
                 .required(),
             password: Joi.string()
-                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+                .pattern(
+                    new RegExp(
+                        '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,12}$'
+                    )
+                )
                 .required(),
         });
         const validationResult = schema.validate(req.body);

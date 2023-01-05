6713c91d2c7aea26f3651be2c7308c4695b7cb13
Roman Serediuk <box4rom@gmail.com>
2023-01-05 11:50:33 +0200

    getTransactionsByDate/ fix query period

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -45,8 +45,8 @@ const getTransactionsByDate = async (req, res) => {
 
     const monthNum = new Date(year, month).getMonth();
     console.log('monthNum: ', monthNum);
-    const dateFrom = Date.parse(new Date(year, monthNum - 1));
-    const dateTo = Date.parse(new Date(year, monthNum));
+    const dateFrom = Date.parse(new Date(year, monthNum));
+    const dateTo = Date.parse(new Date(year, monthNum + 1));
 
     const transactions = await getByDate(_id, dateFrom, dateTo);
 

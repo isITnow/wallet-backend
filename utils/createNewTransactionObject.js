35e47ebb78e0e3e1061fe59771cfef7de7e9d4de
Roman Serediuk <box4rom@gmail.com>
2023-01-05 20:24:59 +0200

    validate new transaction date

--- utils/createNewTransactionObject.js
+++ utils/createNewTransactionObject.js
@@ -1,6 +1,15 @@
 const createNewTransactionObject = (data, userId, balance) => {
     const { type, category = 'income', amount, date, comments = '' } = data;
 
+    const today = new Date();
+    const tomorrow = today.setDate(today.getDate() + 1);
+    const tomorrowZeroHours = new Date(tomorrow).setHours(0, 0, 0, 0);
+    const isInvalidDate = date >= tomorrowZeroHours;
+
+    if (isInvalidDate) {
+        return false;
+    }
+
     let actualBalance =
         type === 'income'
             ? (balance + amount).toFixed(2)
